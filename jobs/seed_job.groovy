a16408990e60c44827014f7eb695fda8d6dd053f
Santiago Montoya <guarambosmg@gmail.com>
2019-09-30 16:23:50 -0500

    seed Job.groovy

---
+++ jobs/seed_job.groovy
@@ -0,0 +1,30 @@
+project_name = "Actividad-2"
+url_repo = "git@github.com:Guarambo/seed-job-dsl.git"
+repo_name = "seed-job"
+
+
+folder('Actividad-2') {
+    displayName('Actividad 2')
+    description('Actividad con WAS')
+}
+
+pipelineJob(project_name) {
+    definition {
+        triggers {
+            scm('H/1 * * * *')
+        }
+
+        cpsScn{
+            scm {
+                git {
+                    remote {
+                        name(repo_name)
+                        url(url_repo)
+                    }
+                }
+                scriptPath("Jenkinsfile")
+            }
+        }
+    }
+}
+
