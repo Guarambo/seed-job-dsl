c143b35584b1afc04019e872b3acc8c5e56a57c9
Santiago Montoya <guarambosmg@gmail.com>
2019-10-07 10:46:01 -0500

    creating folder and pipeline for Java practice

--- jobs/seed_job.groovy
+++ jobs/seed_job.groovy
@@ -1,5 +1,6 @@
 //folder_name = "Act-2"
 project_name = "Act-2/Actividad-2"
+project_name_java = "practica-java-devops/prueba"
 url_repo = "git@github.com:Guarambo/seed-job-dsl.git"
 repo_name = "seed-job"
 
@@ -7,6 +8,11 @@ repo_name = "seed-job"
 folder('Act-2') {
     displayName('Actividad 2')
     description('Actividad con WAS')
+}
+
+folder('practica-java-devops') {
+    displayName('practica Java')
+    description('prueba de Jenkinsfile')
 }
 
 pipelineJob(project_name) {
@@ -24,6 +30,26 @@ pipelineJob(project_name) {
                     }
                 }
                 scriptPath("Jenkinsfile")
+            }
+        }
+    }
+}
+
+pipelineJob(project_name_java) {
+    definition {
+        triggers {
+            scm('H/1 * * * *')
+        }
+
+        cpsScm{
+            scm {
+                git {
+                    remote {
+                        name(repo_name)
+                        url(url_repo)
+                    }
+                }
+                scriptPath("practica-java-devops/Jenkinsfile")
             }
         }
     }
